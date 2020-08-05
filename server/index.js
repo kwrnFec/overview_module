bdd69bc421e59e6119fa25607a44a167dd849944
Honorio <htaveras90@gmail.com>
2020-08-05 17:10:57 -0600

    update: more repo setup

--- server/index.js
+++ server/index.js
@@ -1,11 +1,14 @@
 const express = require('express');
+
 const app = express();
 const bodyParser = require('body-parser');
+const path = require('path');
+
 const port = 3333;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static(__dirname + '/../client/dist'));
+app.use(express.static(path.join(__dirname, '/../client/dist')));
 
 app.get('/', (req, res) => {});
 
