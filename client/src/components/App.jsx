a25e0886499d5f16ae9972f14f1a9e8ecf6f9eeb
Honorio <htaveras90@gmail.com>
2020-08-07 14:42:53 -0600

    update: completed main basic grid layout

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import { Container, Row, Col } from 'react-bootstrap';
+import { Container, Row, Col, Navbar } from 'react-bootstrap';
 import Navibar from './Logo-And-Searchbar/Navibar.jsx';
 
 class App extends Component {
@@ -11,7 +11,23 @@ class App extends Component {
 
   render() {
     return (
-
+      <Container className='full-container'>
+        <Navibar />
+        <Container className='announcement-message'>
+          STATE-WIDE ANNOUNCEMENT MESSAGE! —— SALE / DISCOUNT{' '}
+          <strong>OFFER</strong> —— <u>NEW PRODUCT HIGHTLIGHT</u>
+        </Container>
+        <Container className='main-container'>
+          <Container className='image-gallery'>Image-gallery</Container>
+          <Container>
+            <Container className='product-information'>
+              Product information
+            </Container>
+            <Container className="style-selector">Style selector</Container>
+            <Container className="add-to-cart">Add to Cart</Container>
+          </Container>
+        </Container>
+      </Container>
     );
   }
 }
