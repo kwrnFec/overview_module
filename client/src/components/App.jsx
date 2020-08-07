52e8d68127244848e576c681eec7b8fec34114c8
Honorio <htaveras90@gmail.com>
2020-08-07 11:20:45 -0600

    starting grid from scratch

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -11,33 +11,7 @@ class App extends Component {
 
   render() {
     return (
-      <Container className='container' fluid>
-        <Row className='row-zero'>
-          <Col>
-            <Navibar />
-          </Col>
-          {/* <div>
-            <p>
-              SITE-WIDE ANNOUNCEMENTS MESSAGE! —— SALE / DISCOUNT{' '}
-              <strong>OFFER</strong> —— <u>NEW PRODUCT HIGHLIGHT</u>
-            </p>
-          </div> */}
-        </Row>
-        <Row className='row-one' xl={9}>
-          <Col className='col image-gallery-col' md={9}>
-            image gallery
-          </Col>
-          <Col className='col star-ratings-col' md={3}>
-            star ratings
-          </Col>
-        </Row>
 
-        <Row>
-          <Col>
-            CATEGORY
-          </Col>
-        </Row>
-      </Container>
     );
   }
 }
