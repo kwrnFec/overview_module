a6c9a104862cd379f00061b5dd350b38ccaa346d
Honorio <htaveras90@gmail.com>
2020-08-08 15:12:37 -0600

    update: complete product list get requests

--- server/index.js
+++ server/index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const cors = require('cors');
 const axios = require('axios').default;
 
-const url = 'http://52.26.193.201:3000';
 const app = express();
 const bodyParser = require('body-parser');
 const path = require('path');
 
 const port = 3333;
+const url = 'http://52.26.193.201:3000';
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -16,9 +16,25 @@ app.use(cors());
 
 app.get('/products/list', (req, res) => {
   axios.get(`${url}/products/list`)
-    .then((response) => {
-      res.send(response.data);
-    })
+    .then((response) => res.status(200).send(response.data))
+    .catch((err) => res.status(500).send(err));
+});
+
+app.get('/products/:product_id/', (req, res) => {
+  axios.get(`${url}/products/${req.params.product_id}`)
+    .then((response) => res.status(200).send(response.data))
+    .catch((err) => res.status(500).send(err));
+});
+
+app.get('/products/:product_id/styles', (req, res) => {
+  axios.get(`${url}/products/${req.params.product_id}/styles`)
+    .then((response) => res.status(200).send(response.data))
+    .catch((err) => res.status(500).send(err));
+});
+
+app.get('/products/:product_id/related', (req, res) => {
+  axios.get(`${url}/products/${req.params.product_id}/related`)
+    .then((response) => res.status(200).send(response.data))
     .catch((err) => res.status(500).send(err));
 });
 
