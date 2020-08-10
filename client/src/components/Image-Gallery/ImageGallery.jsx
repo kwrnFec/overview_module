6213bc1a7efc1023bd1bc8e1dedc72fdc35544b7
Honorio <htaveras90@gmail.com>
2020-08-10 14:48:23 -0600

    update: carousel rendering images from api, random id

--- client/src/components/Image-Gallery/ImageGallery.jsx
+++ client/src/components/Image-Gallery/ImageGallery.jsx
@@ -5,71 +5,42 @@ import Axios from 'axios';
 const ControlledCarousel = () => {
   const [index, setIndex] = useState(0);
   const [isLoaded, setIsLoaded] = useState(false);
-  const [productId, setProductId] = useState(1);
+  const [productId, setProductId] = useState([]);
   const [image, setImage] = useState([]);
 
   const handleSelect = (selectedIndex, e) => {
     setIndex(selectedIndex);
   };
 
-  // console.log("setProd: ", setProductId(productId));
-
-
   useEffect(() => {
-    Axios.get(`http://52.26.193.201:3000/products/${Math.floor(Math.random() * 100)}/styles`)
+    Axios.get(
+      `http://52.26.193.201:3000/products/${Math.floor(Math.random() * 1000)}/styles`)
       .then((res) => {
-        console.log('RES DATA IMG GAL: ', res.data);
+        console.log('RES DATA IMG GAL: ', res.data.results);
         setIsLoaded(true);
-        setProductId(res.data);
+        setProductId(res.data.results);
       },
       (err) => {
         setIsLoaded(true);
         console.log(err);
-      },
+      }
       );
   }, []);
 
-  // while there is
-
   return (
-    <Carousel activeIndex={index} onSelect={handleSelect}>
-      <Carousel.Item>
-        <img
-          className='d-block w-100'
-          src='https://preview.redd.it/ew85u34inn951.png?width=960&crop=smart&auto=webp&s=bfb28f0196407e6217258d3cbcb47df5c0828a54'
-          alt='First slide'
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className='d-block w-100'
-          src='https://features.japantimes.co.jp/wp-content/uploads/2019/02/tokyoknights-slide4.jpg'
-          alt='Second slide'
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
-      </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className='d-block w-100'
-          src='https://features.japantimes.co.jp/wp-content/uploads/2019/02/tokyoknights-slide1.jpg'
-          alt='Third slide'
-        />
-
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
-      </Carousel.Item>
+    <Carousel
+      autoPlay={false}
+      interval={null}
+      activeIndex={index}
+      onSelect={handleSelect}
+    >
+      {productId.map((product) =>
+        product.photos.map((photo) => (
+          <Carousel.Item>
+            <img className='d-block w-100' src={photo.url} alt='First slide' />
+          </Carousel.Item>
+        ))
+      )}
     </Carousel>
   );
 };
