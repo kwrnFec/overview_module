98305e5c2bd2e4b1c4a779879a936dc9d0b35b06
Honorio <htaveras90@gmail.com>
2020-08-15 21:14:52 -0600

    update: functioning size and qty selectors

--- client/src/components/Shopping-Cart/ShoppingCart.jsx
+++ client/src/components/Shopping-Cart/ShoppingCart.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-nested-ternary */
 import React, { useState } from 'react';
 import { Container, Row, Col, DropdownButton, Dropdown } from 'react-bootstrap';
 
@@ -6,6 +7,7 @@ const ShoppingCart = ({ currentStyle, styleSkus }) => {
   const [wasClicked, setWasClicked] = useState(false);
   const [selectedSize, setSelectedSize] = useState(null);
   const [selectedQuantity, setSelectedQuantity] = useState(0);
+  const [qtyIdx, setQtyIdx] = useState(0);
 
   let inStock = true;
   Object.keys(styleSkus).forEach((sku) => {
@@ -26,6 +28,26 @@ const ShoppingCart = ({ currentStyle, styleSkus }) => {
     quantityList.push(i + 1);
   }
 
+  const handleQuantitySelect = (eKey) => {
+    setQtyIdx(quantityList[eKey]);
+  };
+
+  const selectSizeTitle = () => (
+    selectedSize || (inStock ? 'SELECT SIZE' : 'OUT OF STOCK')
+  );
+
+  const selectQuantityTitle = () => (
+    quantityList.length === 0
+      ? '-'
+      : !qtyIdx
+        ? 1
+        : qtyIdx > quantityList.length
+          ? quantityList[quantityList.length - 1]
+          : qtyIdx < quantityList.length
+            ? qtyIdx
+            : qtyIdx
+  );
+
   return (
     <Container fluid className="shopping-cart-container">
       <Row xs={3}>
@@ -33,28 +55,26 @@ const ShoppingCart = ({ currentStyle, styleSkus }) => {
           <DropdownButton
             bsPrefix="cart-select-size"
             id="dropdown-basic-button dropdown-chevron"
-            title={selectedSize || (inStock ? 'SELECT SIZE' : 'OUT OF STOCK')}
+            title={selectSizeTitle()}
           >
-            {
-              Object.entries(styleSkus).map((size, quantity) => quantity > 0 && (
-                <Dropdown.Item
-                  onClick={() => clickedAndSelectSku(size[0], quantity)}
-                >
-                  {size[0]}
-                </Dropdown.Item>
-              ))
-            }
+            {Object.entries(styleSkus).map((size, quantity) => quantity > 0 && (
+              <Dropdown.Item onClick={() => clickedAndSelectSku(size[0], quantity)}>
+                {size[0]}
+              </Dropdown.Item>
+            ))}
           </DropdownButton>
         </Col>
         <Col>
           <DropdownButton
             bsPrefix="cart-select-quantity"
             id="dropdown-basic-button"
-            title="1"
+            title={selectQuantityTitle()}
           >
-            <Dropdown.Item>Action</Dropdown.Item>
-            <Dropdown.Item>Another action</Dropdown.Item>
-            <Dropdown.Item>Something else</Dropdown.Item>
+            {quantityList.map((quantity, i) => (
+              <Dropdown.Item eventKey={i} onSelect={handleQuantitySelect}>
+                {quantity}
+              </Dropdown.Item>
+            ))}
           </DropdownButton>
         </Col>
       </Row>
