50592eb3221355820b219c6b9e7b1689d37cc3a9
Honorio <htaveras90@gmail.com>
2020-08-21 18:43:34 -0600

    update: set up for proxy server working, ci before I dive into zoom feature

--- client/src/components/Image-Gallery/ImageGallery.jsx
+++ client/src/components/Image-Gallery/ImageGallery.jsx
@@ -3,6 +3,8 @@ import Carousel from 'react-bootstrap/Carousel';
 import Axios from 'axios';
 import { render } from 'enzyme';
 
+// import Crop from '../../../media/Crop.png';
+
 const ControlledCarousel = ({ currentStyle }) => {
   const [index, setIndex] = useState(0);
 
@@ -12,12 +14,14 @@ const ControlledCarousel = ({ currentStyle }) => {
 
   const renderThumbnails = () => (currentStyle.photos && (
     currentStyle.photos.map((thumbnail, i) => (
-      <img
-        className="d-block thumbnail-image"
-        src={thumbnail.url}
-        alt="thumbnail"
-        onClick={() => setIndex(i)}
-      />
+      <div className="thumnails-list">
+        <img
+          className="d-block thumbnail-image"
+          src={thumbnail.url}
+          alt="thumbnail"
+          onClick={() => setIndex(i)}
+        />
+      </div>
     ))
   ));
 
@@ -43,9 +47,16 @@ const ControlledCarousel = ({ currentStyle }) => {
           ))
         )}
       </Carousel>
+      {/* <button type="submit">
+        <img
+          id="zoom-icon"
+          src={Crop}
+          alt="zoom"
+        />
+      </button> */}
 
       <div className="thumbnails-container">
-        <div className="thumnails-list">{renderThumbnails()}</div>
+        <div>{renderThumbnails()}</div>
       </div>
     </div>
   );
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -4,4 +4,4 @@ import Bootstrap from 'react-bootstrap';
 import App from './components/App.jsx';
 import '../../bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<App />, document.getElementById('product-detail'));
+ReactDOM.render(<App />, document.getElementById('ovModule'));
